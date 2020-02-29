6c633e34bc0155a6fecfb9bc18263ba5f3bfd469
Leo <a13824172847@gmail.com>
2020-02-29 19:56:37 +0800

    Configuration change

--- build/CMakeFiles/3.15.3/CMakeSystem.cmake
+++ build/CMakeFiles/3.15.3/CMakeSystem.cmake
@@ -1,13 +1,13 @@
-set(CMAKE_HOST_SYSTEM "Windows-10.0.17763")
+set(CMAKE_HOST_SYSTEM "Windows-10.0.17134")
 set(CMAKE_HOST_SYSTEM_NAME "Windows")
-set(CMAKE_HOST_SYSTEM_VERSION "10.0.17763")
+set(CMAKE_HOST_SYSTEM_VERSION "10.0.17134")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
 
-include("D:/vcpkg/vcpkg/scripts/buildsystems/vcpkg.cmake")
+include("F:/vcpkg/scripts/buildsystems/vcpkg.cmake")
 
-set(CMAKE_SYSTEM "Windows-10.0.17763")
+set(CMAKE_SYSTEM "Windows-10.0.17134")
 set(CMAKE_SYSTEM_NAME "Windows")
-set(CMAKE_SYSTEM_VERSION "10.0.17763")
+set(CMAKE_SYSTEM_VERSION "10.0.17134")
 set(CMAKE_SYSTEM_PROCESSOR "AMD64")
 
 set(CMAKE_CROSSCOMPILING "FALSE")
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: F:/Experiment/OpenGL/final term/期末大作业/期末大作业/大作业代码
+# Install script for directory: F:/CG/code/code
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "F:/Experiment/OpenGL/final term/期末大作业/期末大作业/大作业代码/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "F:/CG/code/code/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
